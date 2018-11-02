b133f2ef3e28350b630e49c7f108776c21d056fa
liuqi[刘琪] <liuqi17@xiaomi.com>
2018-11-02 17:11:58 +0800

    fix: fix example

--- router/example.js
+++ router/example.js
@@ -1,30 +1,28 @@
 const router = require('express').Router()
 const mock = require('mockjs').mock
 /**
- * @api {get} /example// 使用示例
+ * @api {get} /example/ 使用示例
  * @apiGroup example
- * @apiParam {Number} id 用户id
- * @apiSuccess {Object} micoin 金币对象
- * @apiSuccess {Number} micoin.cur_coin 当前金币
- * @apiSuccess {Number} micoin.last_coin 剩余金币
- * @apiSuccess {Array} micoin_record 金币记录
- * @apiSuccess {String} micoin_record.show_str 金币操作
- * @apiSuccess {String} micoin_record.time_str 时间
- * @apiSuccess {Number} micoin_record.number 金币数量
- * @apiSuccess {Object} prize_record 兑换记录
- * @apiSuccess {String} prize_record.img 商品图片链接
- * @apiSuccess {String} prize_record.time_str 兑换时间
- * @apiSuccess {String} prize_record.name 产品名称
- * @apiSampleRequest http://localhost:3000/example//
+ * @apiParam {String} userId 用户id
+ * @apiSuccess {String} userName 用户名
+ * @apiSuccess {String} userId 用户id
+ * @apiSuccess {String} birthDate 用户生日
+ * @apiSuccess {String} avatar 用户头像
+ * @apiSuccess {Number} age 用户年龄
+ * @apiSuccess {Array} record 兑换记录
+ * @apiSuccess {String} record.name 商品名称
+ * @apiSuccess {String} record.method 兑换方式
+ * @apiSuccess {String} record.time 兑换时间
+ * @apiSampleRequest http://localhost:3000/example/
  * @apiVersion 1.0.0
  */
-router.get('//', function (req, res, next) {
+router.get('/', function (req, res, next) {
   let _jsonpcallback = req.query.jsonpcallback
   if (_jsonpcallback) {
     res.type('text/javascript')
-    res.send(_jsonpcallback + '(' + JSON.stringify(mock({"message":"ok","code":0,"data":{"userName":"@cname()","userId":"@id()","birthDate":"@date('yyyy-MM-dd')","avatar":"@image(100x100, #3399ff)","sign":"@csentence()","age|1-100":100,"record|1-20":[{"method":"@pick('Check-in', 'Lucky Draw', 'Exchange', 'Official reward')","time":"@datetime()"}]}})) + ')')
+    res.send(_jsonpcallback + '(' + JSON.stringify(mock({"message":"ok","code":0,"data":{"userName":"@cname()","userId":"@id()","birthDate":"@date('yyyy-MM-dd')","avatar":"@image(100x100, #3399ff)","sign":"@csentence()","age|1-100":100,"record|1-20":[{"name":"@pick('IPhone X', 'IPhone XS', 'Macbook Pro')","method":"@pick('Check-in', 'Lucky Draw', 'Exchange', 'Official reward')","time":"@datetime()"}]}})) + ')')
   } else {
-    res.json(mock({"message":"ok","code":0,"data":{"userName":"@cname()","userId":"@id()","birthDate":"@date('yyyy-MM-dd')","avatar":"@image(100x100, #3399ff)","sign":"@csentence()","age|1-100":100,"record|1-20":[{"method":"@pick('Check-in', 'Lucky Draw', 'Exchange', 'Official reward')","time":"@datetime()"}]}}))
+    res.json(mock({"message":"ok","code":0,"data":{"userName":"@cname()","userId":"@id()","birthDate":"@date('yyyy-MM-dd')","avatar":"@image(100x100, #3399ff)","sign":"@csentence()","age|1-100":100,"record|1-20":[{"name":"@pick('IPhone X', 'IPhone XS', 'Macbook Pro')","method":"@pick('Check-in', 'Lucky Draw', 'Exchange', 'Official reward')","time":"@datetime()"}]}}))
   }
 })
 
